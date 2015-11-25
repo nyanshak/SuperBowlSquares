34240c68225e0da3835e3115e8a599cb0b808743
Brendan Shaklovitz <bshaklovitz@atlassian.com>
2015-11-25 13:56:17 -0600

    update

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ This is a python flask app to manage creating / managing Super Bowl Squares.
 It is intended to manage a single game for each year. 
 
 # Installation
-mkvirtualenv squares
+
+```bash
+virtualenv .env
+source .env/bin/activate
 pip install -r requirements.txt
-(set up config.py)
 python run.py
+```
--- squares_website/static/js/main.js
+++ squares_website/static/js/main.js
@@ -1,50 +1,48 @@
-
-
 $(function() {
 
-  var gameId = $('input:hidden[name=gameId]').val();
+  var gameId = $('input:hidden[name=gameId]').val()
 
   $(".td-noname, .td-named, .td-verified").hover(function(){
-    $(this).addClass("hover");
+    $(this).addClass("hover")
   }, function(){
-    $(this).removeClass("hover");
-  });
+    $(this).removeClass("hover")
+  })
 
   $.getJSON("/rest/game/" + gameId, function(data) {
     if (data["success"]) {
        game = data["game"]
        afc_team = game["afc_team"]
        if (typeof afc_team == 'string') {
-           $("#afc-label").text(afc_team);
+           $("#afc-label").text(afc_team)
        }
        nfc_team = game["nfc_team"]
        if (typeof nfc_team == 'string') {
-           $("#nfc-label").text(nfc_team);
+           $("#nfc-label").text(nfc_team)
        }
-       afc_values = game["afc_values"];
+       afc_values = game["afc_values"]
        if (afc_values !== null && afc_values.length == 10) {
            for (i = 0; i < 10; i++) {
                val = afc_values[i]
-               $("[data-afc='" + i + "']").html(val);
+               $("[data-afc='" + i + "']").html(val)
            }
        }
 
-       nfc_values = game["nfc_values"];
+       nfc_values = game["nfc_values"]
        if (nfc_values !== null && nfc_values.length == 10) {
            for (i = 0; i < 10; i++) {
                val = nfc_values[i]
-               $("[data-nfc='" + i + "']").html(val);
+               $("[data-nfc='" + i + "']").html(val)
            }
        }
        for (i = 0; i < game["squares"].length; i++) {
-           n = game["squares"][i]["name"];
+           n = game["squares"][i]["name"]
            if (typeof n == 'string' && n.length > 0) {
-               square = $("[data-square-pos='" + i + "'");
-               square.text(n);
+               square = $("[data-square-pos='" + i + "'")
+               square.text(n)
                if (game["squares"][i]["verified"]) {
-                   square.addClass("td-verified").removeClass("td-noname");
+                   square.addClass("td-verified").removeClass("td-noname")
                } else {
-                   square.addClass("td-named").removeClass("td-noname");
+                   square.addClass("td-named").removeClass("td-noname")
                }
            }
        }
@@ -56,6 +54,6 @@ $(function() {
     window.setTimeout(function(){ window.location.href = url }, 5000)
     $("#middle").html('<h1>Error</h1><p>That game does not exist. Click <a href="' + url + '">here</a> to make a new game.')
     $("#game-div").show()
-  });
+  })
   
-});
+})
